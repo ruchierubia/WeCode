dfab8f067376be7d55003c8c50e15491f7dd4e63
ruchierubia <ruch.rubi@gmail.com>
2019-08-01 01:45:48 +0800

    Implement ASP NET Core AddMvc vs AddMvcCore

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace WeCode
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //services.AddMvcCore(); not complete, only core services
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
