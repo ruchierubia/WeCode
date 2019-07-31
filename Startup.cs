1c042472c12e00dac9bac044478208f2be1f59bd
ruchierubia <ruch.rubi@gmail.com>
2019-08-01 02:33:40 +0800

    Implement ASP.NET Core dependency injection

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WeCode.Models;
 
 namespace WeCode
 {
@@ -23,8 +24,9 @@ namespace WeCode
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             //services.AddMvcCore(); not complete, only core services
+            services.AddMvc();
+            services.AddSingleton<ITalentRepository, MockTalentRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
