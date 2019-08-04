e9787f19d257ca60dbc7cbcd049272d76eca90de
ruchierubia <ruch.rubi@gmail.com>
2019-08-05 05:21:47 +0800

    Logging Exceptions in ASP.NET Core

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,12 @@ namespace WeCode.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this._logger = logger;
+        }
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
@@ -20,8 +27,8 @@ namespace WeCode.Controllers
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    _logger.LogWarning($"404 Error Occured. Path = {statusCodeResult.OriginalPath} " +
+                        $"and QueryString = {statusCodeResult.OriginalQueryString}");
                     break;
             }
             return View("NotFound");
@@ -31,9 +38,8 @@ namespace WeCode.Controllers
         public IActionResult Error()
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            ViewBag.ExceptionPath = exceptionDetails.Path;
-            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
-            ViewBag.StackTrace = exceptionDetails.Error.StackTrace;
+            _logger.LogError($"The path {exceptionDetails.Path} threw an exception " +
+                $"{exceptionDetails.Error}");
             return View("Error");
         }
     }
