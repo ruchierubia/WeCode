e3407682be54050911a2247e9149f21e34dd496f
ruchierubia <ruch.rubi@gmail.com>
2019-08-07 03:43:31 +0800

    Implement Get list of roles and Edit role in asp net core

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WeCode.Models;
 using WeCode.ViewModels;
 
 namespace WeCode.Controllers
@@ -11,10 +12,12 @@ namespace WeCode.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             this._roleManager = roleManager;
+            this._userManager = userManager;
         }
 
         [HttpGet]
@@ -35,7 +38,7 @@ namespace WeCode.Controllers
                 IdentityResult result = await _roleManager.CreateAsync(identityRole);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("index","home");
+                    return RedirectToAction("ListRoles","Administration");
                 }
                 else
                 {
@@ -46,6 +49,65 @@ namespace WeCode.Controllers
 
                 }
             }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id= {id} cannot be found";
+                return View("NotFound");
+            }
+            var model = new EditRoleViewModel
+            {
+                Id= role.Id,
+                RoleName = role.Name
+
+            };
+            foreach (var user in _userManager.Users)
+            {
+                if(await _userManager.IsInRoleAsync(user,role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id= {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
             return View(model);
         }
     }
