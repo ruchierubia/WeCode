7953c83e2c6c27facb3da43442e87d9fb8ccc14a
ruchierubia <ruch.rubi@gmail.com>
2019-08-08 04:11:05 +0800

    Implement Edit identity user in asp net core

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -33,6 +33,71 @@ namespace WeCode.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = $"User with id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                Address = user.Address,
+                City = user.City,
+                Country = user.Country,
+                Claims = userClaims.Select(x => x.Value).ToList(),
+                Roles = userRoles
+            };
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.Id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.Address = model.Address;
+                user.City = model.City;
+                user.Country = model.Country;
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+
+        }
+
+        [HttpGet]
         public IActionResult CreateRole()
         {
             return View();
